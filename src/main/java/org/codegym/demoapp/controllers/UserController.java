514d468b4e2cc32a7a829f84eadead2bcc3ec6b3
npv34 <phanvanluan611996@gmail.com>
2024-09-13 16:06:47 +0700

    demo data binding

--- src/main/java/org/codegym/demoapp/controllers/UserController.java
+++ src/main/java/org/codegym/demoapp/controllers/UserController.java
@@ -1,23 +1,32 @@
 package org.codegym.demoapp.controllers;
 
 import org.codegym.demoapp.entities.User;
+import org.codegym.demoapp.request.UserRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletContext;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/users")
+
 public class UserController {
     public List<User> listUser;
+    private final ServletContext servletContext;
 
-    public UserController() {
+    public UserController(ServletContext servletContext) {
         listUser = new ArrayList<User>();
         listUser.add(new User(1, "Nam"));
         listUser.add(new User(2, "Huy"));
         listUser.add(new User(5, "Tam"));
+        this.servletContext = servletContext;
     }
 
     @GetMapping("")
@@ -41,10 +50,33 @@ public class UserController {
     }
 
     @PostMapping("/add")
-    public String saveUser(@RequestParam("name") String name) {
+    public String saveUser(@ModelAttribute("userRequest") UserRequest userRequest, BindingResult result) {
+        // xu ly upload file
+        MultipartFile avatar = userRequest.getAvatar();
+        if (avatar != null && !avatar.isEmpty()) {
+            String fileName = avatar.getOriginalFilename();
+            // Lấy đường dẫn thư mục gốc của dự án
+            String uploadPath = servletContext.getRealPath("/WEB-INF/static/uploads/");
+            System.out.println(uploadPath);
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs(); // Create the directory if it doesn't exist
+            }
+
+            assert fileName != null;
+            // Lưu file vào thư mục "resources/uploads" trong dự án
+            File destinationFile = new File(uploadDir, fileName);
+            try {
+                avatar.transferTo(destinationFile);
+                int id = listUser.get(listUser.size() - 1).getId() + 1;
+                User user = new User(id, userRequest.getName(), userRequest.getEmail(), userRequest.getPassword(), fileName);
+                listUser.add(user);
+            }catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
         // xu ly request method POST voi url /users/add
-        User lastUser = listUser.get(listUser.size() - 1);
-        listUser.add(new User(lastUser.getId() + 1 , name));
+        System.out.println(userRequest.getName());
         return "redirect:/users";
     }
 }
--- src/main/java/org/codegym/demoapp/entities/User.java
+++ src/main/java/org/codegym/demoapp/entities/User.java
@@ -3,10 +3,21 @@ package org.codegym.demoapp.entities;
 public class User {
     private int id;
     private String name;
+    private String email;
+    public String password;
+    public String pathAvatar;
 
     public User(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public User(int id, String name, String email, String password, String pathAvatar) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.pathAvatar = pathAvatar;
     }
 
     public int getId() {
@@ -23,5 +34,13 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
